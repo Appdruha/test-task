7d3b37412cc92029a5b42313b077e999a6a87f39
Appdruha <121567209+Appdruha@users.noreply.github.com>
2024-04-13 02:48:57 +0500

    Пробую добавить анимации

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { NavLink, Route, Routes, useLocation } from 'react-router-dom'
+import { Main } from './pages/Main.tsx'
+import { Upload } from './pages/Upload.tsx'
+import styles from './app.module.css'
+import { animated, useTransition } from 'react-spring'
+import { useEffect, useRef } from 'react'
+
 export const App = () => {
+  const location = useLocation()
+
+  const containerWidthRef = useRef(0)
+
+  useEffect(() => {
+    const container = document.querySelector('main')
+    if (container) {
+      containerWidthRef.current = Math.floor(container.getBoundingClientRect().width)
+    }
+  }, [])
+
+  const transitions = useTransition(location, {
+    from: {transform: 'translateX(100%)' },
+    enter: {transform: 'translateX(0%)' },
+    leave: {position: 'absolute', transform: `translateX(-${containerWidthRef.current}px)` },
+  })
 
   return (
-    <>
-    </>
+    <main className={styles.container}>
+      <NavLink to='/'>main</NavLink>
+      <NavLink to='/upload'>upload</NavLink>
+      {transitions((style, item  ) => (
+        <animated.div style={style}>
+          <Routes location={item}>
+            <Route index path='/' element={<Main />} />
+            <Route path='/upload' element={<Upload />} />
+          </Routes>
+        </animated.div>
+      ))}
+    </main>
   )
 }
---
+++ src/pages/Upload.tsx
@@ -0,0 +1,9 @@
+
+
+export const Upload = () => {
+  return (
+    <h1>
+      upload
+    </h1>
+  )
+}
